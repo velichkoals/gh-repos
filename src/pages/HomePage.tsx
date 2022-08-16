52f5a703cd2319fc3224c408db16eddb8eaedd44
ALEXEY <velichkoalsprog@gmail.com>
2022-08-16 16:34:19 +0300

    feat: update searchbar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,14 +4,15 @@ import { useDebounce } from '../hooks/debounce';
 
 export const HomePage = () => {
 	const [search, setSearch] = useState<string>('');
+	const [dropdown, setDropDown] = useState<boolean>(false);
 	const debounced = useDebounce(search);
 	const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
 		skip: debounced.length < 3,
 	});
 
 	useEffect(() => {
-		console.log(debounced);
-	}, [debounced]);
+		setDropDown(debounced.length > 3 && data?.length! > 0);
+	}, [debounced, data]);
 
 	return (
 		<div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -22,15 +23,24 @@ export const HomePage = () => {
 			<div className='relative w-[560px]'>
 				<input
 					type='text'
-					className='border py-2 px-4 w-full h-[35px] mb-2 text-[14px]'
+					className='border-2 rounded-sm py-2 px-4 w-full h-[35px] mb-2 text-[14px] focus:outline-none focus:border-blue-300'
 					placeholder='Search for GitHub username...'
 					value={search}
 					onChange={(e) => setSearch(e.target.value)}
 				/>
-				<div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Consequuntur, sapiente!
-				</div>
+				{dropdown && (
+					<ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll'>
+						{isLoading && <p className='text-center '>Loading...</p>}
+						{data?.map((user) => (
+							<li
+								key={user.id}
+								className='py-2 px-4 hover:bg-gray-400 hover:text-white transition-colors cursor-pointer'
+							>
+								{user.login}
+							</li>
+						))}
+					</ul>
+				)}
 			</div>
 		</div>
 	);
